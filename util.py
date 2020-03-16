7310402f3ec3fab9bda885bd86398d0dd2cca6f5
Yong <qbxlvnf11@naver.com>
2020-03-16 12:34:34 +0900

    Add files via upload

--- util.py
+++ util.py
@@ -12,9 +12,26 @@ def load_list_file(path):
 		dialog_id_list = [l.strip('\n') for l in dialog_id_list]
 		return dialog_id_list
 
-# get train & validate & test data
+# get train & validate & test data, key
 def get_train_valid_test_data(data_json, train_id_list, valid_id_list, test_id_list):
-	train_data = [v for k, v in data_json.items() if k in train_id_list]
-	valid_data = [v for k, v in data_json.items() if k in valid_id_list]
-	test_data = [v for k, v in data_json.items() if k in test_id_list]
-	return train_data, valid_data, test_data
+	train_data = []
+	train_key = []
+	valid_data = []
+	valid_key = []
+	test_data = []
+	test_key = []
+	
+	for k, v in data_json.items():
+		if k in train_id_list:
+			train_key.append(k)
+			train_data.append(v)
+	
+		if k in valid_id_list:
+			valid_key.append(k)
+			valid_data.append(v)
+	
+		if k in test_id_list:
+			test_key.append(k)
+			test_data.append(v)
+	
+	return train_data, train_key, valid_data, valid_key, test_data, test_key
